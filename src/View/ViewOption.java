97cc76c29e35d4b63df2fa8984faf4b0e166509d
serfa groupe <groupe serfa>
2019-11-13 11:09:42 +0100

    intitule remis

--- src/View/ViewOption.java
+++ src/View/ViewOption.java
@@ -44,11 +44,11 @@ public class ViewOption {
         titreOption.setFill (Color.WHITE);
         ///
         ///
-        video = new Text(150, 450,"Audio");
+        video = new Text(150, 450,"Video");
         video.setFont(new Font("Arial", 25));
         video.setFill (Color.WHITE);
         ///
-        audio = new Text(150, 600,"Video");
+        audio = new Text(150, 600,"Audio");
         audio.setFont(new Font("Arial", 25));
         audio.setFill (Color.WHITE);
 
